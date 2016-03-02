6d70135af2f7cc7105725d286e9514bb80af20dc
xescuGC <xescugil@gmail.com>
2016-03-02 18:08:59 +0100

    Added 3 pending tests

---
+++ test/after.js
@@ -0,0 +1,6 @@
+import { describe, it } from 'mocha';
+
+describe("Test 'after'", () => {
+  it('must have a function after the method|attribute');
+});
+
---
+++ test/before.js
@@ -0,0 +1,5 @@
+import { describe, it } from 'mocha';
+
+describe("Test 'before'", () => {
+  it('must have a function before the method|attribute');
+});
